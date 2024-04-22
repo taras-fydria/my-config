b752888617cade621e5c8de342d16d1fc44128a0
TarasFydriaDelphi <Taras.Fydria@udelphi.com>
2024-04-22 22:06:25 +0300

    add rust support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,64 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			inlay_hints = { enabled = true },
-		},
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = true },
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        filetypes = { "rust" },
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>fd", vim.diagnostic.open_float)
-			vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, {})
-		end,
-	},
-	{
-		"smjonas/inc-rename.nvim",
-		config = function()
-			require("inc_rename").setup({})
-		end,
-	},
+        -- root_dir = util.root_pattern("Cargo.toml"),
+        settings = {
+          ['rust_analyzer'] = {
+            cargo = {
+              allFeatures = true
+            }
+          }
+        }
+      })
+
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>fd", vim.diagnostic.open_float)
+      vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, {})
+    end,
+  },
+  {
+    "smjonas/inc-rename.nvim",
+    config = function()
+      require("inc_rename").setup({})
+    end,
+  },
 }
