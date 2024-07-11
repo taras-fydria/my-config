c763adc5382f05a171462405b9e30d9130411944
TarasFydriaDelphi <Taras.Fydria@udelphi.com>
2024-07-11 20:54:37 +0300

    fix icon error

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,55 +1,55 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("neo-tree").setup({
-        default_component_configs = {
-          git_status = {
-            symbols = {
-              -- Change type
-              added = "✚",
-              deleted = "✖",
-              modified = "",
-              renamed = "󰁕",
-              -- Status type
-              untracked = "",
-              ignored = "",
-              unstaged = "󰄱",
-              staged = "",
-              conflict = "",
-            },
-          },
-        },
-      })
-      vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
-      vim.keymap.set("n", "<leader>e", ":Neotree  focus<CR>", {})
-    end,
-  },
-  {
-    "DaikyXendo/nvim-material-icon",
-    config = function()
-      require("nvim-material-icon").setup({
-        override = {
-          zsh = {
-            icon = "",
-            color = "#428850",
-            cterm_color = "65",
-            name = "Zsh",
-          },
-        },
-        -- globally enable different highlight colors per icon (default to true)
-        -- if set to false all icons will have the default icon's color
-        color_icons = true,
-        -- globally enable default icons (default to false)
-        -- will get overriden by `get_icons` option
-        default = true,
-      })
-    end,
-  },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup({
+				default_component_configs = {
+					git_status = {
+						symbols = {
+							-- Change type
+							added = "✚",
+							deleted = "✖",
+							modified = "",
+							renamed = "󰁕",
+							-- Status type
+							untracked = "",
+							ignored = "",
+							unstaged = "󰄱",
+							staged = "",
+							conflict = "",
+						},
+					},
+				},
+			})
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+			vim.keymap.set("n", "<leader>e", ":Neotree  focus<CR>", {})
+		end,
+	},
+	{
+		"DaikyXendo/nvim-material-icon",
+		config = function()
+			require("nvim-web-devicons").setup({
+				override = {
+					zsh = {
+						icon = "",
+						color = "#428850",
+						cterm_color = "65",
+						name = "Zsh",
+					},
+				},
+				-- globally enable different highlight colors per icon (default to true)
+				-- if set to false all icons will have the default icon's color
+				color_icons = true,
+				-- globally enable default icons (default to false)
+				-- will get overriden by `get_icons` option
+				default = true,
+			})
+		end,
+	},
 }
