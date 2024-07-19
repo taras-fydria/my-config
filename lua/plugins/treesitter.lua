74e0a5c7b1007ace5072ded478d56b4f0531627d
TarasFydriaDelphi <Taras.Fydria@udelphi.com>
2024-07-19 16:28:04 +0300

    add color pair

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "p00f/nvim-ts-rainbow",
+  },
   build = ":TSUpdate",
   config = function()
     local configs = require("nvim-treesitter.configs")
@@ -8,6 +11,14 @@ return {
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
+      rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        -- colors = {}, -- table of hex strings
+        -- termcolors = {} -- table of colour name strings
+      },
     })
-  end
+  end,
 }
