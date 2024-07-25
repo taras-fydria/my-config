d067177d228caab8e8233827b71a70f61f0b1189
TarasFydriaDelphi <Taras.Fydria@udelphi.com>
2024-07-25 23:51:56 +0300

    add format on save

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,18 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.number = true
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+	callback = function(args)
+		-- 2
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			-- 3
+			buffer = args.buf,
+			callback = function()
+				-- 4 + 5
+				vim.lsp.buf.format({ async = false, id = args.data.client_id })
+			end,
+		})
+	end,
+})
